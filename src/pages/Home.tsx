e6f4f5180816eb4e316c07488bd10d1daf5509fa
Ruan Chagas <ruanchagas95@gmail.com>
2023-09-22 02:42:05 -0300

    styles: fix size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,11 @@
-import { Container } from '@chakra-ui/react';
 import Header from '../components/Header';
 import Recipes from '../components/Recipes';
 
 export default function Home() {
   return (
-    <Container
-      width={ 360 }
-      height={ 640 }
-    >
+    <>
       <Header />
       <Recipes />
-    </Container>
+    </>
   );
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,7 @@
-import { Container } from '@chakra-ui/react';
 import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <Container
-      width={ 360 }
-      height={ 640 }
-    >
-      <Header />
-    </Container>
+    <Header />
   );
 }
